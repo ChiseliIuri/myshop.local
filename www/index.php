acc6e11b9e483c6a728e190dd5a1dbf58aad11a4
i.chiseli <test@mail.com>
2021-09-14 09:10:26 +0300

    add gitignore

--- www/index.php
+++ www/index.php
@@ -1,18 +1,14 @@
 <?php
 
-
-
 include_once '../config/config.php'; //initializarea setarilor
 include_once '../config/db.php';
 include_once '../library/mainFunctions.php'; //Functii principale
 
-
 //indicam cu ce controller vom lucra
 $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'index';
 
-
 //definim cu ce functie vom lucra:
 $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
-//d($actionName,0);
+//d($actionName,0);git
 
 loadPage($smarty, $controllerName, $actionName);
