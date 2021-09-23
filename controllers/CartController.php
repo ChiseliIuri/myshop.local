c87f80590f42f9b14befab3c307d198efdd80302
ChiseliIuri <ghost.rogue22@gmail.com>
2021-09-23 17:23:35 +0300

    add new files

---
+++ controllers/CartController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Controller de lucru cu cosul de cumparaturi
+ *
+ */
+
+include_once '../models/CategoriesModel.php';
+include_once '../models/ProductsModel.php';
+
+/**
+ * Adaugarea produsului in cos
+ *
+ * @param integer id GET parametru - id-ul produsului adaugat
+ * @return json informatia despre operatiune (succes, cantitatea de elemente in cos)
+ */
+function addtocartAction(){
+
+}
---
+++ www/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Functia adaugarii elementului in cos
+ *
+ * @param integer itemID ID-ul produsului
+ * @return in caz de success se reainoiesc datele despre produs pe pagina
+ */
+function addToCart(itemId){
+
+}
