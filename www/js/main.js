db91cb755a57c72ef1f500e4c9c3ad257b2fee94
ChiseliIuri <ghost.rogue22@gmail.com>
2021-12-07 22:32:54 +0200

    add ajax request to usercontroller and function for collecting data from register form

--- www/js/main.js
+++ www/js/main.js
@@ -49,10 +49,59 @@ function removeFromCart(itemId) {
  *
  * @param itemId id productului
  */
-function conversionPrice(itemId){
-    var newCnt = $('#itemCnt_'+ itemId).val();
-    var itemPrice = $('#itemPrice_'+ itemId).attr('value');
+function conversionPrice(itemId) {
+    var newCnt = $('#itemCnt_' + itemId).val();
+    var itemPrice = $('#itemPrice_' + itemId).attr('value');
     var itemRealPrice = newCnt * itemPrice;
 
-    $('#itemRealPrice_'+itemId).html(itemRealPrice);
+    $('#itemRealPrice_' + itemId).html(itemRealPrice);
+}
+
+/**
+ * Citirea datelor de inregistrare din forma
+ *
+ */
+function getData(obj_form) {
+    var hData = {};
+    $('input, textarea, select', obj_form).each(function () {
+        if (this.name && this.name != '') {
+            hData[this.name] = this.value;
+            console.log('hData[' + this.name + '] = ' + hData[this.name])
+        }
+    })
+    return hData
+}
+
+/**
+ * Inregistrarea unui nou utilizator
+ *
+ */
+
+function registerNewUser() {
+    var postData = getData('#registerBox');
+
+    $.ajax({
+        type: 'POST',
+        async: false,
+        url: "/user/register/",
+        data: postData,
+        dataType: 'json',
+        success: function (data) {
+            if (data['success']) {
+                alert('Successfully Registered')
+
+                //>bloc in coloana din stanga
+                $('#registerBox').hide();
+                // $('#userLink').attr('href', '/user/')
+                // $('#userBox').show()
+
+                //>pagina comenzii
+                // $('#loginBox').hide()
+                // $('#btnSaveOrder').show()
+
+            } else {
+                alert(data['message']+"someMess")
+            }
+        }
+    })
 }
